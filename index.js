8e0929fc562d91d0cc7ae87188aff342b9e2f15c
Ajay Ramachandran <dev@ajay.app>
2019-05-27 15:53:43 -0400

    removed deprecated .send() function

--- index.js
+++ index.js
@@ -34,52 +34,44 @@ app.get('/usage', function (req, res) {
 //success and failed
 app.get('/light1On', function (req, res) {
     energyUsed += light1Usage;
-    res.send(200);
+    res.sendStatus(200);
 });
 app.get('/light1Off', function (req, res) {
     energyUsed -= light1Usage;
-    res.send(200);
+    res.sendStatus(200);
 });
 
 //success and failed
 app.get('/light2On', function (req, res) {
     energyUsed += light2Usage;
-    res.send(200);
+    res.sendStatus(200);
 });
 app.get('/light2Off', function (req, res) {
     energyUsed -= light2Usage;
-    res.send(200);
+    res.sendStatus(200);
 });
 
 //success and failed
 app.get('/light3On', function (req, res) {
     energyUsed += light3Usage;
-    res.send(200);
+    res.sendStatus(200);
 });
 app.get('/light3Off', function (req, res) {
     energyUsed -= light3Usage;
-    res.send(200);
+    res.sendStatus(200);
 });
 
 //success and failed
 app.get('/light4On', function (req, res) {
     energyUsed += light4Usage;
-    res.send(200);
+    res.sendStatus(200);
 });
 app.get('/light4Off', function (req, res) {
     energyUsed -= light4Usage;
-    res.send(200);
+    res.sendStatus(200);
 });
 
 app.get('/reset', function (req, res) {
     energyUsed = 0;
-    res.send(200);
+    res.sendStatus(200);
 });
-
-// //data uploader
-// app.post('/data', function (req, res){
-//     upload(req, res, ".csv", "/data/");
-// });
-// app.post('/photos', function (req, res){
-//     upload(req, res, ".JPG", "/photos/");
-// });
